d25de35638571dd8dde9b993b3ced072d62e452f
Ecorous <ecorous@outlook.com>
2024-02-17 13:43:43 +0000

    add hardware.nix to willow.nix

--- willow.nix
+++ willow.nix
@@ -9,7 +9,7 @@
   
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./willow/hardware.nix
     ];
 
   nix.settings.experimental-features = [ "flakes" "nix-command" ];
