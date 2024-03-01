876925aa6b35c9bdd77ba24469d344e4e69dc723
Ecorous <ecorous@outlook.com>
2024-03-01 18:36:27 +0000

    fix things

--- yggdrasil/system.nix
+++ yggdrasil/system.nix
@@ -4,7 +4,7 @@
   imports = [ # Include the results of the hardware scan.
     ../common/system.nix
     ../common/plasma.nix
-    #./hardware.nix
+    ./hardware.nix
   ];
 
   networking.hostName = "yggdrasil";
